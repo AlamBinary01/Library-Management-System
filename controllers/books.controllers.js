18db5551faaccc062b3f3d43640bc2b83a12b0fb
haseeb332 <f200316@cfd.nu.edu.pk>
2022-11-16 01:59:30 +0500

    Alam

--- controllers/books.controllers.js
+++ controllers/books.controllers.js
@@ -1,40 +1,80 @@
-const connection = require("../config/configs");
+const mysql = require("mysql");
+//connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "crud",
+});
+connection.connect(function (error) {
+  if (error) console.log(error);
+  else console.log(" Poyon Oil");
+});
 
-const add_g = (req, res) => {
-  res.render("book_add", {
-    title: "Add Books",
-  });
-};
 
-const insertBook_p = (req, res) => {
-  let data = {
-    book_id: req.body.book_id,
-    book_name: req.body.book_name,
-    author_name: req.body.author_name,
-    department: req.body.department,
-    rack_no: req.body.rack_no,
-  };
-  let sql = "INSERT INTO BOOK SET ?";
-   connection.query(sql, data, (err, results) => {
+//pagination
+function Pagination (req,res){
+  const dataCountQuery = "SELECT COUNT(*) FROM book";
+  connection.query(dataCountQuery, function (err, result) {
     if (err) throw err;
-    res.redirect("search");
-  });
-};
 
-const showUpdateBook_g = (req, res) => {
-  const book_id = req.params.bookId;
-  let sql = `SELECT * FROM BOOK WHERE book_id =${book_id}`;
-  let query = connection.query(sql, (err, results) => {
-    if (err) throw err;
-    res.render("edit_book", {
-      title: "Edit Books",
-      user: results[0],
+    let dataCount = result[0]["COUNT(*)"];
+    let pageNo = req.query.page ? req.query.page : 1;
+    let dataPerPages = req.query.data ? req.query.data : 3;
+    let startLimit = (pageNo - 1) * dataPerPages;
+    let totalPages = Math.ceil(dataCount / dataPerPages);
+    const Query = `SELECT * FROM book LIMIT ${startLimit}, ${dataPerPages}`;
+    connection.query(Query, function (err, result) {
+      if (err) throw err;
+      res.render("user_index", {
+        user: result,
+        pages: totalPages,
+        CurrentPage: pageNo,
+        lastPage: totalPages,
+        title: "Library Management System",
+      });
     });
   });
-};
+}
 
-const updateBook_p = (req, res) => {
-  const book_id = req.body.book_id;
+
+exports.addBook=(req,res)=>{
+    res.render("book_add", {
+        title: "Add Books",
+      });
+      Pagination();
+}
+// //save
+exports.insertBook=(req,res)=>{
+    let data = {
+        book_id: req.body.book_id,
+        book_name: req.body.book_name,
+        author_name: req.body.author_name,
+        department: req.body.department,
+        rack_no: req.body.rack_no,
+      };
+      let sql = "INSERT INTO BOOK SET ?";
+      let query = connection.query(sql, data, (err, results) => {
+        if (err) throw err;
+        res.redirect("/");
+      });
+}
+//edit
+exports.editBook=(req,res)=>{
+    const book_id = req.params.bookId;
+    let sql = `SELECT * FROM BOOK WHERE book_id =${book_id}`;
+    let query = connection.query(sql, (err, results) => {
+      if (err) throw err;
+      res.render("edit_book", {
+        title: "Edit Books",
+        user: results[0],
+      });
+    });
+}
+
+//update
+exports.update=(req,res)=>{
+    const book_id = req.body.book_id;
   let sql =
     "UPDATE BOOK SET book_id='" +
     req.body.book_id +
@@ -52,27 +92,23 @@ const updateBook_p = (req, res) => {
     if (err) throw err;
     res.redirect("/");
   });
-};
+}
 
-const deleteBook_g = (req, res) => {
-  const book_id = req.params.bookId;
-  let sql = `DELETE FROM BOOK WHERE book_id =${book_id}`;
-  let query = connection.query(sql, (err, results) => {
-    if (err) throw err;
-    res.redirect("/");
-  });
-};
 
-const SearchTitle = (req, res) => {
-  res.render("search", {
-    title: "Library Management",
-  });
-};
+//delete
 
-const searchBook_p = (req, res) => {
-  // const username = req.body.sname;
-  // const dataCountQuery = `SELECT COUNT(*) FROM book where book_id = ${username}`;
-  const dataCountQuery = `SELECT COUNT(*) FROM book`;
+exports.delete=(req,res)=>{
+    const book_id = req.params.bookId;
+    let sql = `DELETE FROM BOOK WHERE book_id =${book_id}`;
+    let query = connection.query(sql, (err, results) => {
+      if (err) throw err;
+      res.redirect("/");
+    }); 
+}
+
+exports.search=(req,res)=>{
+  const username = req.body.sname;
+  const dataCountQuery = `SELECT COUNT(*) FROM book where book_id = ${username}`;
   connection.query(dataCountQuery, function (err, result) {
     if (err) throw err;
 
@@ -82,34 +118,42 @@ const searchBook_p = (req, res) => {
     let startLimit = (pageNo - 1) * dataPerPages;
     let totalPages = Math.ceil(dataCount / dataPerPages);
 
-    // const Query = `SELECT * FROM book where book_id = ${username}`;
-    const Query = `SELECT * FROM book LIMIT ${startLimit}, ${dataPerPages}`;
+    const Query = `SELECT * FROM book where book_id = ${username}`;
     connection.query(Query, function (err, result) {
       if (err) throw err;
       res.render("search", {
-        title: "Library Management",
         user: result,
         pages: totalPages,
         CurrentPage: pageNo,
         lastPage: totalPages,
+        title: "Library Management System",
       });
     });
   });
-};
-const apiData = (req, res) => {
-  let sql = `SELECT * FROM BOOK`;
-  connection.query(sql, (err, results) => {
-    if (!err) res.status(201).json(results);
-  });
-};
+}
 
-module.exports = {
-  add_g,
-  insertBook_p,
-  showUpdateBook_g,
-  updateBook_p,
-  deleteBook_g,
-  searchBook_p,
-  SearchTitle,
-  apiData,
-};
+exports.filter=(req,res)=>{
+  const dataCountQuery = "SELECT COUNT(*) FROM book";
+  connection.query(dataCountQuery, function (err, result) {
+    if (err) throw err;
+
+    let sorting = req.params.sorting;
+    let dataCount = result[0]["COUNT(*)"];
+    let pageNo = req.params.page ? req.params.page : 1;
+    let dataPerPages = req.query.data ? req.query.data : 2;
+    let startLimit = (pageNo - 1) * dataPerPages;
+    let totalPages = Math.ceil(dataCount / dataPerPages);
+
+    const Query = `SELECT * FROM book ORDER BY book_id ${sorting} LIMIT ${startLimit}, ${dataPerPages} `;
+    connection.query(Query, function (err, result) {
+      if (err) throw err;
+      res.render("user_index", {
+        user: result,
+        pages: totalPages,
+        CurrentPage: pageNo,
+        lastPage: totalPages,
+        title: "Library Management System",
+      });
+    });
+  });
+}
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const routes= require("./routers/book.routes");
+const path = require("path");
+const body_parser = require("body-parser");
+const port = process.env.port | 3000;
+app.use(express.static("public"));
+// set view file
+app.set("views", path.join(__dirname, "views"));
+
+// set view engine
+app.set("view engine", "ejs");
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({ extended: false }));
+
+app.use(routes);
+
+//server Listening
+app.listen(port, () => {
+  console.log("Server Listening on http://localhost:3000");
+});
