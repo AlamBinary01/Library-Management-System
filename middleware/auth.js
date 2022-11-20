8c0a9e8bcbff58bf80167151c57961e2bb8a2835
Ali Shafa <f200376@cfd.nu.edu.pk>
2022-11-20 23:45:11 +0500

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,9 @@
 const auth = (req, res, next) => {
     if (!req.session.admin) {
-        console.log("going to admin login");
+      
         return res.redirect("/adminlogin");
     }
-    console.log("going to next");
+    
     next();
 };
 
