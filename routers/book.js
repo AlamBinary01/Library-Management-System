1b085edd78c443e634e1b7bf75906e97ffed1f42
Haseeb Mushtaq <f200316@cfd.nu.edu.pk>
2022-11-26 21:14:47 +0500

    All error solved

--- routers/book.js
+++ routers/book.js
@@ -9,7 +9,7 @@ const router = express.Router();
 const Auth=require("../middleware/auth")
 
 
-//Routes
+//RoutesO
 router.route("/adminlogin").get(bookController.adminlogin_title);
 router.route("/adminlogin").post(bookController.admin_login);
 // router.get("/adminmenu",Auth,bookController.adminMenu);
@@ -24,10 +24,18 @@ router.route("/adminmenu/crud").get(Auth,bookController.pagination);
  router.route("/adminmenu/search").get((req,res)=>{res.render("search")});
 router.route("/adminmennu/search").post(Auth,bookController.searchBook);
 router.route("/adminmenu/Sorting/:sorting/:page").get(Auth,bookController.filterBook);
+router.route("/adminverifycode").get(bookController.admin_verification_code);
+router.route("/adminverifycode").post(bookController.admin_verificode);
+//forget password admin
+router.route("/adminforgetpassword").get(bookController.admin_forgetpassword);
+router.route("/adminforgetpassword").post(bookController.admin_forgetpassworddata);
+//admin forget verification code
+router.route("/adminforgetverificationcode").get(bookController.admin_forgetverificationcode);
+router.route("/adminforgetverificationcode").post(bookController.admin_forgetverificationdata);
 
-// router.route("/adminlogin/add",Auth).get(bookController.addBook_title);
-// router.route("/adminlogin/add",Auth).post(bookController.insertBook);
-// router.route("adminmenu/allBook").get(bookController.AllData);
+// admin update password
+router.route("/adminupdatepassword").get(bookController.admin_updatePasswordfile);
+router.route("/adminupdatepassword").post(bookController.admin_updatepassworddata);
 
 
 
@@ -42,117 +50,6 @@ var transporter = nodemailer.createTransport({
   },
 });
 
-router.get("/forgetpassword", (req, res) => {
-  res.render("forget_pass");
-});
-router.post("/forgetpassword", (req, res) => {
-  let name = req.body.Username;
-  let email = req.body.email;
 
-  let query =
-    "Select * from userdata where username='" +
-    name +
-    "' and email='" +
-    email +
-    "';";
-  //console.log(query);
-  connection.query(query, function (error, result, fields) {
-    if (error) console.log(error);
-    else {
-      if (result.length > 0) {
-        var mailOption = {
-          from: "hase271002@gmail.com",
-          to: email,
-          subject: "Yours Verification Code is:",
-          html:
-            "Your Username is:" + name + " Your Code is:" + result[0].password,
-        };
-        transporter.sendMail(mailOption, function (error, info) {
-          if (error) console.log(error);
-          else console.log("Email has been sent,", info.response);
-        });
-        res.redirect("/");
-      } else {
-        //alert("Login Not Found");
-        console.log("Yahn nhi hona cahye");
-        res.redirect("registration");
-      }
-    }
-  });
-});
-
-///////////Verification////////////////////////
-function generateCode() {
-  var minm = 100000;
-  var maxm = 999999;
-  return Math.floor(Math.random() * (maxm - minm + 1)) + minm;
-}
-
-function verify(emial) {
-  let verificationcode = generateCode();
-  var mailOption = {
-    from: "alambinary01@gmail.com",
-    to: email,
-    subject: "Verification Code",
-    html:
-      "Hello Please Enter this Code to Verify Your Code:" + verificationcode,
-  };
-
-  transporter.sendMail(mailOption, function (error, info) {
-    if (error) {
-      throw error;
-    } else {
-      console.log("I am in verification");
-    }
-  });
-  return true;
-}
-
-router.get("/verify", (req, res) => {
-  res.redirect("verify");
-});
-
-router.post("/verify", (req, res) => {
-  let code = req.body.code;
-  if (code == verificationcode) {
-    console.log("code is correct");
-    res.redirect("/");
-  } else {
-    res.redirect("/verify");
-  }
-});
-////////////Registration///////////
-router.get("/registration", (req, res) => {
-  res.render("registration");
-});
-
-router.post("/registration", (req, res) => {
-  let username = req.body.username;
-  let email = req.body.email;
-  let password = req.body.password;
-
-  connection.query(
-    "INSERT INTO userdata(name,email,pass) VALUES(?,?,?)",
-    [username, email, password],
-    (error, result) => {
-      if (error) {
-        console.log("Error");
-        res.redirect("/welcome");
-      } else {
-        console.log("Data inserted");
-        verify(email);
-        res.redirect("/verify");
-      }
-    }
-  );
-});
-router.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-//////////////////Admin Login?////////////////////
-router.get("adminMenu", (req, res) => {
-  res.render("add");
-});
 
 module.exports = router;
