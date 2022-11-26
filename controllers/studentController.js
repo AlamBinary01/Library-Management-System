40573e0b2134452b55982d849dfcd064223ef188
Haseeb Mushtaq <f200316@cfd.nu.edu.pk>
2022-11-26 18:48:11 +0500

    Admin Authentication Done

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,18 +1,18 @@
 let username;
-let verificationcode=generateCode();
-const nodemailer= require('nodemailer');
-var  transporter= nodemailer.createTransport({
-  host:'smtp.gmail.com',
-  port:587,
-  secure:false,
-  requireTLS:true,
-  auth:{
-      user:"alishafa0376@gmail.com",
-      pass:"ttugjrkymvtfygqp"
-  }
+let verificationcode = generateCode();
+const nodemailer = require("nodemailer");
+var transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: "alishafa0376@gmail.com",
+    pass: "ttugjrkymvtfygqp",
+  },
 });
-path = require('path');
-const { verify } = require('crypto');
+path = require("path");
+const { verify } = require("crypto");
 const mysql = require("mysql");
 const con = mysql.createConnection({
   host: "localhost",
@@ -21,181 +21,189 @@ const con = mysql.createConnection({
   database: "crud",
 });
 
-
-
-module.exports={
-     student_login: (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'student', 'student_login.html'))
-   
+module.exports = {
+  student_login: (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "student", "student_login.html"));
   },
 
   student_signup: (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'student', 'student_signup.html'))
+    res.sendFile(path.join(__dirname, "..", "student", "student_signup.html"));
   },
 
-  login_data: (req,res)=>{
-    login_name=req.body.username;
-     login_pass=req.body.password;
-    
+  login_data: (req, res) => {
+    login_name = req.body.username;
+    login_pass = req.body.password;
+
     // console.log(name);
     // console.log(pass);
-    
-    let query="select * from Student where username = '"+login_name+"' and password = '"+login_pass+"';"
+
+    let query =
+      "select * from Student where username = '" +
+      login_name +
+      "' and password = '" +
+      login_pass +
+      "';";
     console.log(query);
-        con.query(query,function(error,result,fields){
-            if(error){
-    console.log("Error");
-            }
-            
-            else{
-            if(result.length>0){
-              const admin = { username: login_name, password: login_pass };
-              //const admin="123"
-                     req.session.admin = admin;
-                     console.log(req.session.admin);
-                     res.cookie("CurrentRole", "Student");
-               res.sendFile(path.join(__dirname, '..', 'student', 'student_dashboard.html'))
-
-            }
-            else{
-                console.log("Login Not Found");
-                res.sendFile(path.join(__dirname, '..', 'student', 'student_signup.html'))
-
-            }
-    
+    con.query(query, function (error, result, fields) {
+      if (error) {
+        console.log("Error");
+      } else {
+        if (result.length > 0) {
+          const admin = { username: login_name, password: login_pass };
+          //const admin="123"
+          req.session.admin = admin;
+          console.log(req.session.admin);
+          res.cookie("CurrentRole", "Student");
+          res.sendFile(
+            path.join(__dirname, "..", "student", "student_dashboard.html")
+          );
+        } else {
+          console.log("Login Not Found");
+          res.sendFile(
+            path.join(__dirname, "..", "student", "student_signup.html")
+          );
         }
-        })
+      }
+    });
   },
 
-signup_data:(req,res)=>{
-  if(!req.file){
-    console.log("File Not Found");
-}
-//res.sendFile(__dirname+"/Registration.html");
-let name_=req.body.name_;
-let username=req.body.username;
-let email=req.body.email;
-let password=req.body.password;
-let phone=req.body.phone;
-let age=req.body.age;
-let qualification=req.body.qualification;
-let file_name=req.file.originalname;
-console.log(email);
-verification(email);
-//[username,email,password],
-con.query('INSERT INTO student(name,username,email,phonenumber,password,qualification,age,image) VALUES(?,?,?,?,?,?,?,?)',[name_,username,email,phone,password,qualification,age,file_name],(error,result)=>{
-    if(error){
-        console.log("Error");
-       res.redirect("/welcome");
+  signup_data: (req, res) => {
+    if (!req.file) {
+      console.log("File Not Found");
     }
-    else{
-
-        console.log("Data inserted");
-        res.sendFile(path.join(__dirname, '..', 'student', 'verify_code.html'));
-    }
-}) 
-},
-
-verificationcode:(req,res)=>{
-  res.sendFile(path.join(__dirname, '..', 'student', 'verify_code.html'))
-},
-
-verifycode:(req,res)=>{
-  let code =req.body.code;
-  if(code==verificationcode){
-  console.log("code is correct");
-  res.redirect("/studentlogin");
-  }
-  
-  else{
-      res.redirect("/verifycode");
-  }
-},
-
-forgetpassword:(req,res)=>{
-  res.sendFile(path.join(__dirname, '..', 'student', 'forgetpassword.html'))
-},
-
-forgetverificationcode:(req,res)=>{
-  res.sendFile(path.join(__dirname, '..', 'student', 'forgetverificationcode.html'))
-},
-
-forgetverificationdata:(req,res)=>{
-  let code =req.body.code;
-  if(code==verificationcode){
-  console.log("code is correct");
-  res.redirect("/updatepasswordstudent");
-  }
-  
-  else{
-      res.redirect("/forgetverificationcode");
-  }
-},
-
-forgetpassworddata:(req,res)=>{
-  let name=req.body.username;
-  let email=req.body.email;
-  username=name;
-  // console.log(name);
-  // console.log(pass);
-  
-  let query="select * from student where username = '"+name+"' and email = '"+email+"';"
-  console.log(query);
-      con.query(query,function(error,result,fields){
-          if(error){
-  console.log("Error");
-          }
-          
-          else{
-          if(result.length>0){
-verificationcode=generateCode();
-              var mailOption={
-                  from:"alishafa0376@gmail.com",
-                  to:email,
-                  subject:"Verification Code",
-                  html:"Your Verification Password is:"+verificationcode
-              }
-              
-              transporter.sendMail(mailOption,function(error,info){
-                  if(error){ throw error;}
-                  else{ 
-                      console.log("I am in verification");
-              }
-              })
-
-  res.redirect("/forgetverificationcode");
-          }
-          else{
-              console.log("Login Not Found");
-              res.redirect("/studentsignup");
-          }
-  
+    //res.sendFile(__dirname+"/Registration.html");
+    let name_ = req.body.name_;
+    let username = req.body.username;
+    let email = req.body.email;
+    let password = req.body.password;
+    let phone = req.body.phone;
+    let age = req.body.age;
+    let qualification = req.body.qualification;
+    let file_name = req.file.originalname;
+    console.log(email);
+    verification(email);
+    //[username,email,password],
+    con.query(
+      "INSERT INTO student(name,username,email,phonenumber,password,qualification,age,image) VALUES(?,?,?,?,?,?,?,?)",
+      [name_, username, email, phone, password, qualification, age, file_name],
+      (error, result) => {
+        if (error) {
+          console.log("Error");
+          res.redirect("/welcome");
+        } else {
+          console.log("Data inserted");
+          res.sendFile(
+            path.join(__dirname, "..", "student", "verify_code.html")
+          );
+        }
       }
-      })
-},
+    );
+  },
 
+  verificationcode: (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "student", "verify_code.html"));
+  },
 
-updatepasswordfile:(req,res)=>{
-  res.sendFile(path.join(__dirname, '..', 'student', 'updatestudentpassword.html'))
-},
-
-updatepassworddata:(req,res)=>{
-  let pass=req.body.password;
-  let query="update student set password = '"+pass+"' where username = '"+username+"';";
-  console.log(query);
-  con.query(query,(error,result)=>{
-    if(error){
-        console.log("Error");
-       res.redirect("/welcome");
+  verifycode: (req, res) => {
+    let code = req.body.code;
+    if (code == verificationcode) {
+      console.log("code is correct");
+      res.redirect("/studentlogin");
+    } else {
+      res.redirect("/verifycode");
     }
-    else{
+  },
 
+  forgetpassword: (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "student", "forgetpassword.html"));
+  },
+
+  forgetverificationcode: (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "..", "student", "forgetverificationcode.html")
+    );
+  },
+
+  forgetverificationdata: (req, res) => {
+    let code = req.body.code;
+    if (code == verificationcode) {
+      console.log("code is correct");
+      res.redirect("/updatepasswordstudent");
+    } else {
+      res.redirect("/forgetverificationcode");
+    }
+  },
+
+  forgetpassworddata: (req, res) => {
+    let name = req.body.username;
+    let email = req.body.email;
+    username = name;
+    // console.log(name);
+    // console.log(pass);
+
+    let query =
+      "select * from student where username = '" +
+      name +
+      "' and email = '" +
+      email +
+      "';";
+    console.log(query);
+    con.query(query, function (error, result, fields) {
+      if (error) {
+        console.log("Error");
+      } else {
+        if (result.length > 0) {
+          verificationcode = generateCode();
+          var mailOption = {
+            from: "alishafa0376@gmail.com",
+            to: email,
+            subject: "Verification Code",
+            html: "Your Verification Password is:" + verificationcode,
+          };
+
+          transporter.sendMail(mailOption, function (error, info) {
+            if (error) {
+              throw error;
+            } else {
+              console.log("I am in verification");
+            }
+          });
+
+          res.redirect("/forgetverificationcode");
+        } else {
+          console.log("Login Not Found");
+          res.redirect("/studentsignup");
+        }
+      }
+    });
+  },
+
+  updatepasswordfile: (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "..", "student", "updatestudentpassword.html")
+    );
+  },
+
+  updatepassworddata: (req, res) => {
+    let pass = req.body.password;
+    let query =
+      "update student set password = '" +
+      pass +
+      "' where username = '" +
+      username +
+      "';";
+    console.log(query);
+    con.query(query, (error, result) => {
+      if (error) {
+        console.log("Error");
+        res.redirect("/welcome");
+      } else {
         console.log("Data Update");
-        res.redirect("/studentlogin");    }
-}) 
-}
-
-}
+        res.redirect("/studentlogin");
+      }
+    });
+  },
+};
 
 function generateCode() {
   var minm = 100000;
@@ -203,24 +211,24 @@ function generateCode() {
   return Math.floor(Math.random() * (maxm - minm + 1)) + minm;
 }
 
-function verification(email){
+function verification(email) {
   //verificationcode=generateCode();
   //verificationcode=generateCode();
   //verificationcode=generateCode();
-  var mailOption={
-      from:"alishafa0376@gmail.com",
-      to:email,
-      subject:'<h1> Verification Code </h1>',
-      html:"Hello Please Enter this Code to Verify Your Code:"+verificationcode
-  }
-  
-  transporter.sendMail(mailOption,function(error,info){
-      if(error){ throw error;}
-      else{ 
-          console.log("I am in verification");
-  }
-})
-return true;
+  var mailOption = {
+    from: "alishafa0376@gmail.com",
+    to: email,
+    subject: "<h1> Verification Code </h1>",
+    html:
+      "Hello Please Enter this Code to Verify Your Code:" + verificationcode,
+  };
 
+  transporter.sendMail(mailOption, function (error, info) {
+    if (error) {
+      throw error;
+    } else {
+      console.log("I am in verification");
+    }
+  });
+  return true;
 }
-
