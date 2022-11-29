c25228fab6fb1611dbbca975f7fe3da998871c52
Mustajab Ahmad <117346583+MustajabAhmad@users.noreply.github.com>
2022-11-30 00:56:46 +0500

    resolved errors in ADD Fine & Show fine list

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const routes = require("./routers/book");
 const student_routes=require("./routers/studentRouter");
-const fine_routers=require("./routers/fineRouter");
+const fine_routes=require("./routers/fineRouter");
 const path = require("path");
 const body_parser = require("body-parser");
 const port = process.env.port | 3000;
@@ -35,6 +35,7 @@ app.use(body_parser.urlencoded({ extended: false }));
 
 app.use(routes);
 app.use(student_routes);
+app.use(fine_routes);
 
 
 //Diaplay card on page
--- routers/fineRouter.js
+++ routers/fineRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 // const app= express();
 const connection = require("../config/config");
 const nodemailer = require("nodemailer");
-const FineController = require("../controllers/booksControllers");
+const FineController = require("../controllers/fineControllers");
 const router = express.Router();
 const Auth=require("../middleware/auth")
 
@@ -10,8 +10,8 @@ const Auth=require("../middleware/auth")
 router.route("/adminmenu").get(Auth,FineController.adminMenu);
 
 router.route("/adminmenu/addFine").get(Auth,FineController.addFine);
-router.route("/adminmenu/addFine").post(Auth,FineController.addFine);
-router.route("/adminmenu/editFine/:stu_Id").get(Auth,bookController.editBook);
+router.route("/adminmenu/addFine").post(Auth,FineController.insertFine);
+router.route("/adminmenu/editFine/:stu_Id").get(Auth,FineController.editFine);
 router.route("/adminmenu/editFine").post(Auth,FineController.updateFine);
 router.route("/adminmenu/deleteFine/:stu_Id").get(Auth,FineController.deleteFine);
 router.route("/adminmenu/listFine").get(Auth,FineController.listFine);
